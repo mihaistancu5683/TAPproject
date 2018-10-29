afbd8a6e362dcec653f62663198f4798e034cf46
Mihai Stancu <mihaistancu5683@users.noreply.github.com>
2018-10-29 22:20:02 +0200

    Adds random int generator

--- src/com/unibuc/fmi/java/mihaistancu/backtracking.java
+++ src/com/unibuc/fmi/java/mihaistancu/backtracking.java
@@ -1,7 +1,16 @@
 package com.unibuc.fmi.java.mihaistancu;
 
+import java.util.Random;
+
 public class backtracking {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int x = generateRandomIntIntRange(1,9);
+        System.out.println(x);
+    }
+
+
+    public static int generateRandomIntIntRange(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
     }
 }
