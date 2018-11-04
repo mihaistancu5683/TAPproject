7d9b4aeec1278897b7df8c1225ca35a833060092
Mihai <mihaistancu@mail.com>
2018-11-04 22:11:06 +0200

    Adds MergeSort algorithm

--- src/com/unibuc/fmi/java/mihaistancu/MergeSort.java
+++ src/com/unibuc/fmi/java/mihaistancu/MergeSort.java
@@ -9,9 +9,90 @@ public class MergeSort {
         System.out.println();
     }
 
+    // Merges two subarrays of arr[].
+    // First subarray is arr[left..middle]
+    // Second subarray is arr[middle+1..right]
+    public static void merge(int arr[], int left, int middle, int right)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        // Create temp arrays
+        int L[] = new int [n1];
+        int R[] = new int [n2];
+
+        //Copy data to temp arrays
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[middle + 1 + j];
+
+        // Merge the temp arrays
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray array
+        int k = left;
+        while (i < n1 && j < n2)
+        {
+            if (L[i] <= R[j])
+            {
+                arr[k] = L[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[] if any
+        while (i < n1)
+        {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[] if any
+        while (j < n2)
+        {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main MergeSort function
+    public static void sort(int arr[], int left, int right)
+    {
+        if (left < right)
+        {
+            // Find the middle point
+            int middle = (left + right)/2;
+
+            // Sort first and second halves
+            sort(arr, left, middle);
+            sort(arr , middle + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, middle, right);
+        }
+    }
+
     public static void main(String args[])
     {
         int arr[] = {10, 12, 11, 13, 5, 6, 7, 4};
+        System.out.println("Unsorted array:");
+        print(arr);
+
+        sort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array:");
         print(arr);
     }
 }
